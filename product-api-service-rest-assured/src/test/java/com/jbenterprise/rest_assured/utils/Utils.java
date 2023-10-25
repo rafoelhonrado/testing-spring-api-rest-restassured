513bf7f892060f7f365f760420a66d1b576fdb27
principal <rafoelhonrado@hotmail.com>
2023-10-24 21:38:49 -0500

    US-01, RestAssured test for Product API

---
+++ product-api-service-rest-assured/src/test/java/com/jbenterprise/rest_assured/utils/Utils.java
@@ -0,0 +1,15 @@
+package com.jbenterprise.rest_assured.utils;
+
+import com.jbenterprise.rest_assured.entity.ProductRequest;
+
+public class Utils {
+	public static final String USER_AGENT="JBEnterprise";
+	
+    public static ProductRequest generateNewProductRequest() {
+    	return ProductRequest.builder()
+    			.name("Iphone 15")
+    			.description("Un equipo moderno de la marca Apple")
+    			.price(0f)
+    			.build();    	
+    }  
+}
